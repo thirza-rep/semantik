4dbdca79b8a54843c33669789c99606ac57da6b5
Thirza Rep <thirza-rep@users.noreply.github.com>
2025-12-18 02:27:12 +0700

    Fix: Trust all proxies for Vercel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'role' => \App\Http\Middleware\CheckRole::class,
         ]);
